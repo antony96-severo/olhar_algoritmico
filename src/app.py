7bd959d090618eb297536a27ac54786334f7f0aa
Antony Severo <antonysevero1@gmail.com>
2026-05-20 07:56:43 -0300

    Update database configuration in create_app function

--- src/app.py
+++ src/app.py
@@ -6,11 +6,14 @@ from src.controllers.routes import registrar_rotas
 
 def create_app():
     app = Flask(__name__)
+    
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+database_url = os.getenv("DATABASE_URL")
 
-    database_url = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///olhar_algoritmico.db"
-    )
+if database_url:
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] ="sqlite:///olhar_algoritmico.db")
 
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
